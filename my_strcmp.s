ecca0da183410a37e72b389fef1062799cef878a
Никита Рогов <nikitarogov@MacBook-Pro-Nikita.local>
2026-05-04 21:42:48 +0300

    2 part

--- my_strcmp.s
+++ my_strcmp.s
@@ -4,34 +4,17 @@ section .text
 global my_strcmp
 
 my_strcmp:
-    push rbx
+            vmovdqu ymm0, [rdi]
+            vpcmpeqb ymm1, ymm0, [rsi]
+            vpmovmskb eax, ymm1
+            cmp eax, 0FFFFFFFFh
+            jne .not_equal
 
-    xor rax, rax
-    xor rbx, rbx
+            vzeroupper
+            xor eax, eax
+            ret
 
-    .continue:
-
-        inc rbx
-        mov r8, [rdi]
-        mov r9, [rsi]
-
-        cmp r8, r9
-        jne .not_equal
-
-        add rdi, 8
-        add rsi, 8
-
-        cmp rbx, 4
-        jl .continue
-
-        mov rax, 0
-        jmp .end
-
-    .not_equal:
-
-    mov rax, 1
-
-    .end:
-
-    pop rbx
-    ret
+.not_equal:
+            vzeroupper
+            mov eax, 1
+            ret
