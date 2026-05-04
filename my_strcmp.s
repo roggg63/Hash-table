92576e1c2ba0836478dc5091e48bbbc7980c8c7f
Никита Рогов <nikitarogov@MacBook-Pro-Nikita.local>
2026-05-04 21:12:47 +0300

    2 part

--- my_strcmp.s
+++ my_strcmp.s
@@ -1,7 +1,7 @@
-.intel_syntax noprefix
+default rel
 
-.text
-.globl my_strcmp
+section .text
+global my_strcmp
 
 my_strcmp:
     push rbx
@@ -9,29 +9,29 @@ my_strcmp:
     xor rax, rax
     xor rbx, rbx
 
-    continue:
+    .continue:
 
         inc rbx
         mov r8, [rdi]
         mov r9, [rsi]
 
         cmp r8, r9
-        jne not_equal
+        jne .not_equal
 
         add rdi, 8
         add rsi, 8
 
         cmp rbx, 4
-        jl continue
+        jl .continue
 
         mov rax, 0
-        jmp end
+        jmp .end
 
-    not_equal:
+    .not_equal:
 
     mov rax, 1
 
-    end:
+    .end:
 
     pop rbx
     ret
